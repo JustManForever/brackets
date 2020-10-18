8f890884dcc480b55d73aa115dfa137447fefbed
JustManForever <70839428+JustManForever@users.noreply.github.com>
2020-10-18 21:01:23 +0400

    fix: enter correct values

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, config) {
      let arr = config.map((a) =>
         a = a[0] + a[1]
     );
     let i = 0;
     
-    while ( i < str.length/2) {
+    while ( i < str.length + 3) {
         arr.filter((item) => {
             while (str.indexOf(item) !== -1) {
                 str = str.replace(item, "");
