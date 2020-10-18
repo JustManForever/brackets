557dbcab31237cf0d040d0c3a894db1483ae3142
JustManForever <70839428+JustManForever@users.noreply.github.com>
2020-10-18 20:51:09 +0400

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     let arr = config.map((a) =>
+        a = a[0] + a[1]
+    );
+    let i = 0;
+    
+    while ( i < str.length/2) {
+        arr.filter((item) => {
+            while (str.indexOf(item) !== -1) {
+                str = str.replace(item, "");
+                console.log(str)
+            }
+        })
+        i++;
+    }
+    return str.length === 0 ;
 }
